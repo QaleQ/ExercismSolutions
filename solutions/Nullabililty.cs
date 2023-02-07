13c9ac86efaf40dc131f148af75ce37b40ee2deb
QaleQ <aleflard@gmail.com>
2023-02-07 12:07:41 +0200

    Tim from Marketing first iteration

---
+++ solutions/Nullabililty.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+static class Badge
+{
+    public static string Print(int? id, string name, string? department)
+    {
+        string idPart = id == null ? "" : $"[{id}] - ";
+        string departmentPart = department == null ? "OWNER" : department.ToUpper();
+        return $"{idPart}{name} - {departmentPart}";
+    }
+}
