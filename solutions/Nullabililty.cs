46658842fbb09e2d8524b866a3c9953b9a2750c4
QaleQ <aleflard@gmail.com>
2023-02-13 15:25:00 +0200

    Tim from Marketin second iteration

--- solutions/Nullabililty.cs
+++ solutions/Nullabililty.cs
@@ -1,11 +1,5 @@
-﻿using System;
-
-static class Badge
+﻿static class Badge
 {
-    public static string Print(int? id, string name, string? department)
-    {
-        string idPart = id == null ? "" : $"[{id}] - ";
-        string departmentPart = department == null ? "OWNER" : department.ToUpper();
-        return $"{idPart}{name} - {departmentPart}";
-    }
+    public static string Print(int? id, string name, string? department) => 
+        $"{(id == null ? "" : $"[{id}] - ")}{name} - {department?.ToUpper() ?? "OWNER"}";
 }
