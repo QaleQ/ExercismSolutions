64c2b089ae5a19eb2c5fbc4d2a373bb9f16e6c38
QaleQ <aleflard@gmail.com>
2023-06-19 17:53:04 +0300

    ElonsToys.cs

---
+++ solutions/ElonsToys.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class RemoteControlCar
+{
+    int battery = 100;
+    int distance;
+    
+    public static RemoteControlCar Buy() => new();
+    public string DistanceDisplay() => $"Driven {distance} meters";
+    public string BatteryDisplay() => battery > 0 ? $"Battery at {battery}%" : "Battery empty";
+    public void Drive()
+    {
+        if (battery > 0)
+        {
+            distance += 20;
+            battery -= 1;
+        }
+    }
+}
