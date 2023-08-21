98eb3e91343134f096d76a502dcad8dd71cbc9db
QaleQ <aleflard@gmail.com>
2023-08-21 16:34:55 +0300

    PigLatin second iteration

--- solutions/PigLatin.cs
+++ solutions/PigLatin.cs
@@ -2,6 +2,9 @@
 
 public static class PigLatin
 {
+    static string[] startVowelSounds = {"a", "e", "i", "o", "u", "yt", "xr"};
+    static string otherVowelSounds = "aeiouy";
+    
     public static string Translate(string word)
     {
         string[] words = word.Split(" ");
@@ -12,27 +15,19 @@ public static class PigLatin
 
     static string ConvertWord(string word)
     {
-        if (word[0] is 'a' or 'e' or 'i' or 'o' or 'u' || word.StartsWith("xr") || word.StartsWith("yt"))
+        foreach (string sound in startVowelSounds) if (word.StartsWith(sound))
             return word + "ay";
-        else if (word.StartsWith("squ") || word.StartsWith("thr") || word.StartsWith("sch"))
-        {
-            string add = word.Remove(3);
-            word = word.Remove(0, 3);
-            word += add;
-        }
-        else if (word.StartsWith("ch") || word.StartsWith("sh") || word.StartsWith("qu") || word.StartsWith("th") ||
-                 word.StartsWith("rh"))
-        {
-            string add = word.Remove(2);
-            word = word.Remove(0, 2);
-            word += add;
-        }
-        else
-        {
-            word += word[0];
-            word = word.Remove(0, 1);
-        }
 
-        return word + "ay";
+        int charsToMove = 1;
+        for (int i = 1; i < word.Length; ++i)
+        {
+            if (otherVowelSounds.Contains(word[i]))
+            {
+                if (word[i] == 'u' && word[i - 1] == 'q') charsToMove++;
+                break;
+            }
+            charsToMove++;
+        }
+        return word.Remove(0, charsToMove) + word.Remove(charsToMove) + "ay";
     }
 }
